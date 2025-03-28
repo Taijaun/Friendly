1bf3b497baeb13b3cb60275348cbd7f9453d628e
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-03-28 14:37:59 +0000

    Addded default users to preview struct

--- Friendly/ContentView.swift
+++ Friendly/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    var users = [User]()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +22,14 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ContentView(users: [
+        User(id: "1", isActive: true, name: "Bob", age:18, Company: "Google", email: "googledefault@gmail.com", address: "64 Zoo Lane", about: "I'm a fake google employee and I like cheese", registered: .distantPast, tags: [
+            "Cheese",
+            "Beans",
+            "Toast"
+        ], friends: [
+            Friend(id: "55", name: "George"),
+            Friend(id: "66", name: "Steve")
+        ])
+    ])
 }
