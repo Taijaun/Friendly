6c7b8717a13a5897d27148330f97296438ade88b
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-03-28 14:10:23 +0000

    First commit

---
+++ Friendly/FriendlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendlyApp.swift
+//  Friendly
+//
+//  Created by Taijaun Pitt on 28/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct FriendlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
